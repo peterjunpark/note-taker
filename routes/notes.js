9a5e916bf58cc8ca6b7b0aeaa61826b2e7b53ac0
Peter Park <peterjpark@pm.me>
2023-05-17 23:39:09 -0400

    Add DELETE logic and crypto for UUID generation

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,18 @@
 const notes = require("express").Router();
 const { writeFile } = require("fs");
-const data = require("../db/db.json");
+const { randomUUID } = require("crypto");
+let data = require("../db/db.json");
+
+const addUUID = (obj) => {
+  obj.id = randomUUID();
+  return obj;
+};
+
+const updateDB = (data) => {
+  writeFile("./db/db.json", JSON.stringify(data), (err) => {
+    if (err) console.error(err);
+  });
+};
 
 // /api/notes
 notes.get("/", (req, res) => {
@@ -8,15 +20,16 @@ notes.get("/", (req, res) => {
 });
 
 notes.post("/", (req, res) => {
-  data.push(req.body);
-
-  writeFile("./db/db.json", JSON.stringify(data), (err) => {
-    if (err) console.error(err);
-  });
-
+  data.push(addUUID(req.body));
+  updateDB(data);
   res.status(201).json(data);
 });
 
-notes.delete("/:id", (req, res) => {});
+notes.delete("/:id", (req, res) => {
+  data = data.filter((note) => note.id !== req.params.id);
+  updateDB(data);
+  console.log(data);
+  res.json(data);
+});
 
 module.exports = notes;
